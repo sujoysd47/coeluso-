8c11c42af2b58fc02e47ecc0dd40cd06dcfdbd38
sujoysd47 <84784127+sujoysd47@users.noreply.github.com>
2021-09-13 21:16:42 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  //wow
+  wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+//scroll
+
+scroller.init();
+
+
+
+});
+
